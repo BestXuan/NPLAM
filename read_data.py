0cd2675162193dc57ae0cd0881fcaa4473676d1f
BestXuan <46296340+BestXuan@users.noreply.github.com>
2022-10-18 11:01:45 +0800

    Update read_data.py

--- read_data.py
+++ read_data.py
@@ -1,12 +1,14 @@
 import numpy as np
 
+
+
 def simulation_data_30(random_lambda):
     # read data
     read_data = np.load('./datasets/simulation_data_30_data.npy')
     np.random.seed(random_lambda)
     np.random.shuffle(read_data)
     read_data = read_data.T #(31.1600)
-    # Max-Min
+    # maxmin
     for i in range(read_data.shape[0]):
         temp_max = np.max(read_data[i])
         temp_min = np.min(read_data[i])
@@ -35,14 +37,52 @@ def simulation_data_30(random_lambda):
     x_test = test_set.T[:-1].T
     y_test = test_set.T[-1].T
     return x_train,y_train,x_val,y_val,x_test,y_test
+def simulation_data_sm(random_lambda, sm):
+    # read data
+    read_data = np.load('./datasets/simulation_data_30_data_highsample.npy')
+    np.random.seed(random_lambda)
+    np.random.shuffle(read_data)
+    read_data = read_data.T #(31.1600)
+    # maxmin
+    for i in range(read_data.shape[0]):
+        temp_max = np.max(read_data[i])
+        temp_min = np.min(read_data[i])
+        for j in range(read_data.shape[1]):
+            if (temp_max - temp_min) < 0.1:
+                read_data[i][j] = 0
+            else:
+                read_data[i][j] = -1 + 2 * (read_data[i][j] - temp_min) / (temp_max - temp_min)
+    # #Z-score
+    # for i in range(read_data.shape[0]):
+    #     temp_max = np.max(read_data[i])
+    #     temp_min = np.min(read_data[i])
+    #     for j in range(read_data.shape[1]):
+    #         if (temp_max - temp_min) < 0.1:
+    #             read_data[i][j] = 0
+    #         else:
+    #             read_data[i][j] = -1 + 2 * (read_data[i][j] - temp_min) / (temp_max - temp_min)
+    read_data = read_data.T
+    train_set = read_data[:111111]
+    val_set = read_data[111111:111431]
+    test_set = read_data[111431:]
+    x_train = train_set.T[:-1].T
+    y_train = train_set.T[-1].T
+    x_val = val_set.T[:-1].T
+    y_val = val_set.T[-1].T
+    x_test = test_set.T[:-1].T
+    y_test = test_set.T[-1].T
 
+    x_train = x_train[:sm]
+    y_train = y_train[:sm]
+
+    return x_train,y_train,x_val,y_val,x_test,y_test
 def simulation_data_300(random_lambda):
     # read data
     read_data = np.load('./datasets/simulation_data_300_data.npy')
     np.random.seed(random_lambda)
     np.random.shuffle(read_data)
     read_data = read_data.T  # (31.16000)
-    # Max-Min
+    # maxmin
     for i in range(read_data.shape[0]):
         temp_max = np.max(read_data[i])
         temp_min = np.min(read_data[i])
@@ -72,6 +112,112 @@ def simulation_data_300(random_lambda):
     x_test = test_set.T[:-1].T
     y_test = test_set.T[-1].T
     return x_train, y_train, x_val, y_val, x_test, y_test
+def simulation_data_new_alllinear(random_lambda):
+    # read data
+    read_data = np.load('./datasets/new_all_linear_30.npy')
+    np.random.seed(random_lambda)
+    np.random.shuffle(read_data)
+    read_data = read_data.T #(31.1600)
+    # maxmin
+    for i in range(read_data.shape[0]):
+        temp_max = np.max(read_data[i])
+        temp_min = np.min(read_data[i])
+        for j in range(read_data.shape[1]):
+            if (temp_max - temp_min) < 0.1:
+                read_data[i][j] = 0
+            else:
+                read_data[i][j] = -1 + 2 * (read_data[i][j] - temp_min) / (temp_max - temp_min)
+    # #Z-score
+    # for i in range(read_data.shape[0]):
+    #     temp_max = np.max(read_data[i])
+    #     temp_min = np.min(read_data[i])
+    #     for j in range(read_data.shape[1]):
+    #         if (temp_max - temp_min) < 0.1:
+    #             read_data[i][j] = 0
+    #         else:
+    #             read_data[i][j] = -1 + 2 * (read_data[i][j] - temp_min) / (temp_max - temp_min)
+    read_data = read_data.T
+    train_set = read_data[:960]
+    val_set = read_data[960:1280]
+    test_set = read_data[1280:]
+    x_train = train_set.T[:-1].T
+    y_train = train_set.T[-1].T
+    x_val = val_set.T[:-1].T
+    y_val = val_set.T[-1].T
+    x_test = test_set.T[:-1].T
+    y_test = test_set.T[-1].T
+    return x_train,y_train,x_val,y_val,x_test,y_test
+def simulation_data_new_nonelinear(random_lambda):
+    # read data
+    read_data = np.load('./datasets/new_none_linear_30.npy')
+    np.random.seed(random_lambda)
+    np.random.shuffle(read_data)
+    read_data = read_data.T #(31.1600)
+    # maxmin
+    for i in range(read_data.shape[0]):
+        temp_max = np.max(read_data[i])
+        temp_min = np.min(read_data[i])
+        for j in range(read_data.shape[1]):
+            if (temp_max - temp_min) < 0.1:
+                read_data[i][j] = 0
+            else:
+                read_data[i][j] = -1 + 2 * (read_data[i][j] - temp_min) / (temp_max - temp_min)
+    # #Z-score
+    # for i in range(read_data.shape[0]):
+    #     temp_max = np.max(read_data[i])
+    #     temp_min = np.min(read_data[i])
+    #     for j in range(read_data.shape[1]):
+    #         if (temp_max - temp_min) < 0.1:
+    #             read_data[i][j] = 0
+    #         else:
+    #             read_data[i][j] = -1 + 2 * (read_data[i][j] - temp_min) / (temp_max - temp_min)
+    read_data = read_data.T
+    train_set = read_data[:960]
+    val_set = read_data[960:1280]
+    test_set = read_data[1280:]
+    x_train = train_set.T[:-1].T
+    y_train = train_set.T[-1].T
+    x_val = val_set.T[:-1].T
+    y_val = val_set.T[-1].T
+    x_test = test_set.T[:-1].T
+    y_test = test_set.T[-1].T
+    return x_train,y_train,x_val,y_val,x_test,y_test
+def simulation_data_new_partlinear(random_lambda):
+    # read data
+    read_data = np.load('./datasets/new_part_linear_30.npy')
+    np.random.seed(random_lambda)
+    np.random.shuffle(read_data)
+    read_data = read_data.T #(31.1600)
+    # maxmin
+    for i in range(read_data.shape[0]):
+        temp_max = np.max(read_data[i])
+        temp_min = np.min(read_data[i])
+        for j in range(read_data.shape[1]):
+            if (temp_max - temp_min) < 0.1:
+                read_data[i][j] = 0
+            else:
+                read_data[i][j] = -1 + 2 * (read_data[i][j] - temp_min) / (temp_max - temp_min)
+    # #Z-score
+    # for i in range(read_data.shape[0]):
+    #     temp_max = np.max(read_data[i])
+    #     temp_min = np.min(read_data[i])
+    #     for j in range(read_data.shape[1]):
+    #         if (temp_max - temp_min) < 0.1:
+    #             read_data[i][j] = 0
+    #         else:
+    #             read_data[i][j] = -1 + 2 * (read_data[i][j] - temp_min) / (temp_max - temp_min)
+    read_data = read_data.T
+    train_set = read_data[:960]
+    val_set = read_data[960:1280]
+    test_set = read_data[1280:]
+    x_train = train_set.T[:-1].T
+    y_train = train_set.T[-1].T
+    x_val = val_set.T[:-1].T
+    y_val = val_set.T[-1].T
+    x_test = test_set.T[:-1].T
+    y_test = test_set.T[-1].T
+    return x_train,y_train,x_val,y_val,x_test,y_test
+
 
 def simulation_data_CH(random_lambda):
     # read data
@@ -102,7 +248,6 @@ def simulation_data_CH(random_lambda):
     x_test = data[:-1].T[16512:]
     y_test = data[-1].T[16512:].reshape((20640 - 16512), 1)
     return x_train, y_train, x_val, y_val, x_test, y_test
-
 def simulation_data_BH(random_lambda):
     # read data
     path_1 = r'./datasets/boston_house_prices.csv'
@@ -133,111 +278,35 @@ def simulation_data_BH(random_lambda):
     x_test = data[:-1].T[404:506]
     y_test = data[-1].T[404:506].reshape(102, 1)
     return x_train, y_train, x_val, y_val, x_test, y_test
-
-def simulation_data_new_alllinear(random_lambda):
+def simulation_data_SC(random_lambda):
     # read data
-    read_data = np.load('./datasets/new_all_linear_30.npy')
+    path_1 = r'./datasets/superconductivty.csv'
+    file = open(path_1,'r')
+    file_data = file.readlines()
+    file_data = file_data[1:]
+    for i in range(len(file_data)):
+        file_data[i] = file_data[i].split(',')
+        file_data[i][-1] = file_data[i][-1][:-1]
+    data = []
+    for i in range(len(file_data)):
+        data.append([])
+        for j in range(82):
+            data[i].append(float(file_data[i][j]))
+    data = np.asarray(data)
     np.random.seed(random_lambda)
-    np.random.shuffle(read_data)
-    read_data = read_data.T #(31.1600)
-    # Max-Min
-    for i in range(read_data.shape[0]):
-        temp_max = np.max(read_data[i])
-        temp_min = np.min(read_data[i])
-        for j in range(read_data.shape[1]):
-            if (temp_max - temp_min) < 0.1:
-                read_data[i][j] = 0
-            else:
-                read_data[i][j] = -1 + 2 * (read_data[i][j] - temp_min) / (temp_max - temp_min)
-    # #Z-score
-    # for i in range(read_data.shape[0]):
-    #     temp_max = np.max(read_data[i])
-    #     temp_min = np.min(read_data[i])
-    #     for j in range(read_data.shape[1]):
-    #         if (temp_max - temp_min) < 0.1:
-    #             read_data[i][j] = 0
-    #         else:
-    #             read_data[i][j] = -1 + 2 * (read_data[i][j] - temp_min) / (temp_max - temp_min)
-    read_data = read_data.T
-    train_set = read_data[:960]
-    val_set = read_data[960:1280]
-    test_set = read_data[1280:]
-    x_train = train_set.T[:-1].T
-    y_train = train_set.T[-1].T
-    x_val = val_set.T[:-1].T
-    y_val = val_set.T[-1].T
-    x_test = test_set.T[:-1].T
-    y_test = test_set.T[-1].T
-    return x_train,y_train,x_val,y_val,x_test,y_test
+    np.random.shuffle(data)
+    data = data.T
+    for i in range(data.shape[0]):
+        data_max_train = np.max(data[i])
+        data_min_train = np.min(data[i])
+        for j in range(data.shape[1]):
+            data[i][j] = -1 + (data[i][j] - data_min_train) * (2 / (data_max_train - data_min_train))
+    x_train = data[:-1].T[:12757]
+    y_train = data[-1].T[:12757].reshape(12757, 1)
+    x_val = data[:-1].T[12757:17010]
+    y_val = data[-1].T[12757:17010].reshape(4253, 1)
+    x_test = data[:-1].T[17010:]
+    y_test = data[-1].T[17010:].reshape(4253, 1)
+    return x_train, y_train, x_val, y_val, x_test, y_test
 
-def simulation_data_new_nonelinear(random_lambda):
-    # read data
-    read_data = np.load('./datasets/new_none_linear_30.npy')
-    np.random.seed(random_lambda)
-    np.random.shuffle(read_data)
-    read_data = read_data.T #(31.1600)
-    # Max-Min
-    for i in range(read_data.shape[0]):
-        temp_max = np.max(read_data[i])
-        temp_min = np.min(read_data[i])
-        for j in range(read_data.shape[1]):
-            if (temp_max - temp_min) < 0.1:
-                read_data[i][j] = 0
-            else:
-                read_data[i][j] = -1 + 2 * (read_data[i][j] - temp_min) / (temp_max - temp_min)
-    # #Z-score
-    # for i in range(read_data.shape[0]):
-    #     temp_max = np.max(read_data[i])
-    #     temp_min = np.min(read_data[i])
-    #     for j in range(read_data.shape[1]):
-    #         if (temp_max - temp_min) < 0.1:
-    #             read_data[i][j] = 0
-    #         else:
-    #             read_data[i][j] = -1 + 2 * (read_data[i][j] - temp_min) / (temp_max - temp_min)
-    read_data = read_data.T
-    train_set = read_data[:960]
-    val_set = read_data[960:1280]
-    test_set = read_data[1280:]
-    x_train = train_set.T[:-1].T
-    y_train = train_set.T[-1].T
-    x_val = val_set.T[:-1].T
-    y_val = val_set.T[-1].T
-    x_test = test_set.T[:-1].T
-    y_test = test_set.T[-1].T
-    return x_train,y_train,x_val,y_val,x_test,y_test
 
-def simulation_data_new_partlinear(random_lambda):
-    # read data
-    read_data = np.load('./datasets/new_part_linear_30.npy')
-    np.random.seed(random_lambda)
-    np.random.shuffle(read_data)
-    read_data = read_data.T #(31.1600)
-    # Max-Min
-    for i in range(read_data.shape[0]):
-        temp_max = np.max(read_data[i])
-        temp_min = np.min(read_data[i])
-        for j in range(read_data.shape[1]):
-            if (temp_max - temp_min) < 0.1:
-                read_data[i][j] = 0
-            else:
-                read_data[i][j] = -1 + 2 * (read_data[i][j] - temp_min) / (temp_max - temp_min)
-    # #Z-score
-    # for i in range(read_data.shape[0]):
-    #     temp_max = np.max(read_data[i])
-    #     temp_min = np.min(read_data[i])
-    #     for j in range(read_data.shape[1]):
-    #         if (temp_max - temp_min) < 0.1:
-    #             read_data[i][j] = 0
-    #         else:
-    #             read_data[i][j] = -1 + 2 * (read_data[i][j] - temp_min) / (temp_max - temp_min)
-    read_data = read_data.T
-    train_set = read_data[:960]
-    val_set = read_data[960:1280]
-    test_set = read_data[1280:]
-    x_train = train_set.T[:-1].T
-    y_train = train_set.T[-1].T
-    x_val = val_set.T[:-1].T
-    y_val = val_set.T[-1].T
-    x_test = test_set.T[:-1].T
-    y_test = test_set.T[-1].T
-    return x_train,y_train,x_val,y_val,x_test,y_test
