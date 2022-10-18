97b42fd6e5f7cbf61f02e0ae2d37359dfdbc033c
BestXuan <46296340+BestXuan@users.noreply.github.com>
2022-10-18 11:02:17 +0800

    Update layer.py

--- layer.py
+++ layer.py
@@ -1,6 +1,12 @@
+import time
+import numpy as np
+import os
+import matplotlib.pyplot as plt
 import tensorflow as tf
+from tensorflow.keras.models import load_model
 from tensorflow.python.eager import context
 from tensorflow.python.framework import dtypes
+from tensorflow.python.framework import ops
 from tensorflow.python.framework import tensor_shape
 from tensorflow.python.keras import activations
 from tensorflow.python.keras import backend as K
@@ -9,14 +15,28 @@ from tensorflow.python.keras import initializers
 from tensorflow.python.keras import regularizers
 from tensorflow.python.keras.engine.base_layer import Layer
 from tensorflow.python.keras.engine.input_spec import InputSpec
+from tensorflow.python.keras.utils import conv_utils
+from tensorflow.python.keras.utils import generic_utils
+from tensorflow.python.keras.utils import tf_utils
+from tensorflow.python.ops import array_ops
 from tensorflow.python.ops import gen_math_ops
 from tensorflow.python.ops import math_ops
 from tensorflow.python.ops import nn
 from tensorflow.python.ops import sparse_ops
 from tensorflow.python.ops import standard_ops
+from tensorflow.python.ops import variable_scope
+from tensorflow.python.util import nest
+from tensorflow.python.util import tf_inspect
+from tensorflow.python.util.tf_export import keras_export
+from sklearn.metrics import mean_squared_error
+import warnings
+import random
 
-class Dense_l1_sv(tf.keras.layers.Layer):
-    #this is the key layer of Feature Selection gate.
+
+
+
+class Dense_sv(tf.keras.layers.Layer):
+    # feature selection gates
     def __init__(self,
                  units,
                  activation=None,
@@ -30,12 +50,10 @@ class Dense_l1_sv(tf.keras.layers.Layer):
                  bias_constraint=None,
                  w_train=True,
                  b_train=True,
-                 same_a=False,
-                 same_a_a=None,
                  **kwargs):
         if 'input_shape' not in kwargs and 'input_dim' in kwargs:
             kwargs['input_shape'] = (kwargs.pop('input_dim'),)
-        super(Dense_l1_sv, self).__init__(activity_regularizer=regularizers.get(activity_regularizer), **kwargs)
+        super(Dense_sv, self).__init__(activity_regularizer=regularizers.get(activity_regularizer), **kwargs)
         self.units = int(units) if not isinstance(units, int) else units
         self.activation = activations.get(activation)
         self.use_bias = use_bias
@@ -47,8 +65,6 @@ class Dense_l1_sv(tf.keras.layers.Layer):
         self.bias_constraint = constraints.get(bias_constraint)
         self.w_train = w_train
         self.b_train = b_train
-        self.same_a = same_a
-        self.same_a_a = same_a_a
         self.supports_masking = True
         self.input_spec = InputSpec(min_ndim=2)
 
@@ -63,17 +79,14 @@ class Dense_l1_sv(tf.keras.layers.Layer):
                              'should be defined. Found `None`.')
         last_dim = tensor_shape.dimension_value(input_shape[-1])
         self.input_spec = InputSpec(min_ndim=2, axes={-1: last_dim})
-        if self.same_a is True:
-            self.kernel = self.same_a_a
-        else:
-            self.kernel = self.add_weight(
-                'kernel',
-                shape=[1, self.units],
-                initializer=self.kernel_initializer,
-                regularizer=self.kernel_regularizer,
-                constraint=self.kernel_constraint,
-                dtype=self.dtype,
-                trainable=self.w_train)
+        self.kernel = self.add_weight(
+            'kernel',
+            shape=[1, self.units],
+            initializer=self.kernel_initializer,
+            regularizer=self.kernel_regularizer,
+            constraint=self.kernel_constraint,
+            dtype=self.dtype,
+            trainable=self.w_train)
         if self.use_bias:
             self.bias = self.add_weight(
                 'bias',
@@ -94,13 +107,12 @@ class Dense_l1_sv(tf.keras.layers.Layer):
         if self.activation is not None:
             return tf.split(tf.transpose(self.activation(outputs)), self.units)
         return tf.split(tf.transpose(outputs), self.units)
-
-class Dense_l1_sd_3(Layer):
-    #this is the key layer of Structure Discovery gate.
+class Dense_sd(Layer):
+    # structure discovery gates
     def __init__(self,
-                 units=1,
-                 activation=None,
-                 use_bias=True,
+                 units=1,  # 结点个数
+                 activation=None,  # 用什么激活函数
+                 use_bias=True,  # 是否用偏差
                  kernel_initializer='glorot_uniform',
                  bias_initializer='zeros',
                  kernel_regularizer=None,
@@ -110,12 +122,10 @@ class Dense_l1_sd_3(Layer):
                  bias_constraint=None,
                  w_train=True,
                  b_train=True,
-                 same_a=False,
-                 same_a_a=None,
                  **kwargs):
         if 'input_shape' not in kwargs and 'input_dim' in kwargs:
             kwargs['input_shape'] = (kwargs.pop('input_dim'),)
-        super(Dense_l1_sd_3, self).__init__(
+        super(Dense_sd, self).__init__(
             activity_regularizer=regularizers.get(activity_regularizer), **kwargs)
         self.units = int(units) if not isinstance(units, int) else units
         self.activation = activations.get(activation)
@@ -128,10 +138,8 @@ class Dense_l1_sd_3(Layer):
         self.bias_constraint = constraints.get(bias_constraint)
         self.w_train = w_train
         self.b_train = b_train
-        self.same_a = same_a
-        self.same_a_a = same_a_a
-        self.supports_masking = True
-        self.input_spec = InputSpec(min_ndim=2)
+        self.supports_masking = True  # 这个不知道是干嘛的，我选择忽略
+        self.input_spec = InputSpec(min_ndim=2)  # 这个不知道是干嘛的，我选择忽略
 
     def build(self, input_shape):
         dtype = dtypes.as_dtype(self.dtype or K.floatx())
@@ -142,19 +150,16 @@ class Dense_l1_sd_3(Layer):
         if tensor_shape.dimension_value(input_shape[-1]) is None:
             raise ValueError('The last dimension of the inputs to `Dense` '
                              'should be defined. Found `None`.')
-        last_dim = tensor_shape.dimension_value(input_shape[-1])
+        last_dim = tensor_shape.dimension_value(input_shape[-1])  # 这个就是inputs的输入的结点个数
         self.input_spec = InputSpec(min_ndim=2, axes={-1: last_dim})
-        if self.same_a is True:
-            self.kernel = self.same_a_a
-        else:
-            self.kernel = self.add_weight(
-                'kernel',
-                shape=[1, self.units],
-                initializer=self.kernel_initializer,
-                regularizer=self.kernel_regularizer,
-                constraint=self.kernel_constraint,
-                dtype=self.dtype,
-                trainable=self.w_train)
+        self.kernel = self.add_weight(
+            'kernel',
+            shape=[1, self.units],
+            initializer=self.kernel_initializer,
+            regularizer=self.kernel_regularizer,
+            constraint=self.kernel_constraint,
+            dtype=self.dtype,
+            trainable=self.w_train)
         if self.use_bias:
             self.bias = self.add_weight(
                 'bias',
@@ -170,91 +175,11 @@ class Dense_l1_sd_3(Layer):
 
     def call(self, inputs):
         self.kernel_up = self.kernel
+        # self.kernel_down = 1 - self.kernel_up
         self.kernel_down = 1
         up_outputs = gen_math_ops.mul(inputs, self.kernel_up)
         down_outputs = gen_math_ops.mul(inputs, self.kernel_down)
         return up_outputs, down_outputs
-
-class Dense_split(Layer):
-    # this is the key layer of split the feature.
-    def __init__(self,
-                 units,
-                 activation=None,
-                 use_bias=True,
-                 kernel_initializer='glorot_uniform',
-                 bias_initializer='zeros',
-                 kernel_regularizer=None,
-                 bias_regularizer=None,
-                 activity_regularizer=None,
-                 kernel_constraint=None,
-                 bias_constraint=None,
-                 w_train=True,
-                 b_train=True,
-                 same_a=False,
-                 same_a_a=None,
-                 **kwargs):
-        if 'input_shape' not in kwargs and 'input_dim' in kwargs:
-            kwargs['input_shape'] = (kwargs.pop('input_dim'),)
-        super(Dense_split, self).__init__(activity_regularizer=regularizers.get(activity_regularizer), **kwargs)
-        self.units = int(units) if not isinstance(units, int) else units
-        self.activation = activations.get(activation)
-        self.use_bias = use_bias
-        self.kernel_initializer = initializers.get(kernel_initializer)
-        self.bias_initializer = initializers.get(bias_initializer)
-        self.kernel_regularizer = regularizers.get(kernel_regularizer)
-        self.bias_regularizer = regularizers.get(bias_regularizer)
-        self.kernel_constraint = constraints.get(kernel_constraint)
-        self.bias_constraint = constraints.get(bias_constraint)
-        self.w_train = w_train
-        self.b_train = b_train
-        self.same_a = same_a
-        self.same_a_a = same_a_a
-        self.supports_masking = True
-        self.input_spec = InputSpec(min_ndim=2)
-
-    def build(self, input_shape):
-        dtype = dtypes.as_dtype(self.dtype or K.floatx())
-        if not (dtype.is_floating or dtype.is_complex):
-            raise TypeError('Unable to build `Dense` layer with non-floating point '
-                            'dtype %s' % (dtype,))
-        input_shape = tensor_shape.TensorShape(input_shape)
-        if tensor_shape.dimension_value(input_shape[-1]) is None:
-            raise ValueError('The last dimension of the inputs to `Dense` '
-                             'should be defined. Found `None`.')
-        last_dim = tensor_shape.dimension_value(input_shape[-1])
-        self.input_spec = InputSpec(min_ndim=2, axes={-1: last_dim})
-        if self.same_a is True:
-            self.kernel = self.same_a_a
-        else:
-            self.kernel = self.add_weight(
-                'kernel',
-                shape=[1, self.units],
-                initializer=self.kernel_initializer,
-                regularizer=self.kernel_regularizer,
-                constraint=self.kernel_constraint,
-                dtype=self.dtype,
-                trainable=self.w_train)
-        if self.use_bias:
-            self.bias = self.add_weight(
-                'bias',
-                shape=[self.units, ],
-                initializer=self.bias_initializer,
-                regularizer=self.bias_regularizer,
-                constraint=self.bias_constraint,
-                dtype=self.dtype,
-                trainable=self.b_train)
-        else:
-            self.bias = None
-        self.built = True
-
-    def call(self, inputs):
-        outputs = gen_math_ops.mul(inputs, self.kernel)
-        if self.use_bias:
-            outputs = nn.bias_add(outputs, self.bias)
-        if self.activation is not None:
-            return tf.split(tf.transpose(self.activation(outputs)), self.units)
-        return tf.split(tf.transpose(outputs), self.units)
-
 class Dense(Layer):
   def __init__(self,
                units,
@@ -269,8 +194,6 @@ class Dense(Layer):
                bias_constraint=None,
                w_train=True,
                b_train=True,
-               same_a = False,
-               same_a_a = None,
                **kwargs):
     if 'input_shape' not in kwargs and 'input_dim' in kwargs:
       kwargs['input_shape'] = (kwargs.pop('input_dim'),)
@@ -287,8 +210,6 @@ class Dense(Layer):
     self.bias_constraint = constraints.get(bias_constraint)
     self.w_train = w_train
     self.b_train = b_train
-    self.same_a = same_a
-    self.same_a_a = same_a_a
     self.supports_masking = True
     self.input_spec = InputSpec(min_ndim=2)
   def build(self, input_shape):
@@ -302,17 +223,14 @@ class Dense(Layer):
                        'should be defined. Found `None`.')
     last_dim = tensor_shape.dimension_value(input_shape[-1])
     self.input_spec = InputSpec(min_ndim=2, axes={-1: last_dim})
-    if self.same_a is True:
-        self.kernel = self.same_a_a
-    else:
-        self.kernel = self.add_weight(
-            'kernel',
-            shape=[last_dim, self.units],
-            initializer=self.kernel_initializer,
-            regularizer=self.kernel_regularizer,
-            constraint=self.kernel_constraint,
-            dtype=self.dtype,
-            trainable=self.w_train)
+    self.kernel = self.add_weight(
+        'kernel',
+        shape=[last_dim, self.units],
+        initializer=self.kernel_initializer,
+        regularizer=self.kernel_regularizer,
+        constraint=self.kernel_constraint,
+        dtype=self.dtype,
+        trainable=self.w_train)
     if self.use_bias:
       self.bias = self.add_weight(
           'bias',
